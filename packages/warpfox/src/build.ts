b0bef6a1c2d9c71d33df2ca633cb0254ef452103
mr. M <mauro.balades@tutanota.com>
2025-03-23 22:00:36 +0100

    Add download and unpack functionality for Firefox source; update package dependencies

--- packages/warpfox/src/build.ts
+++ packages/warpfox/src/build.ts
@@ -1,35 +1,117 @@
 import type Cache from "./@types/cache";
 import type Manifest from "./@types/manifest";
-import { Downloader } from "nodejs-file-downloader";
-import ProgressBar from "progress";
 import { join } from "path";
-import { exists } from "node:fs/promises";
+import { exists, mkdir } from "node:fs/promises";
 import logger from "./logger";
+import { commandExistsSync } from "./utils/command-exists";
+import { downloadFileToLocation } from "./utils/download";
+import { resolve } from "node:path";
+import { execa } from "execa";
 
 async function downloadFirefox(version: string) {
   const url = `https://github.com/mauro-balades/warpfox/releases/download/firefox-v${version}/firefox-source.tar.gz`;
-  if (await exists(join(process.cwd(), ".warpfox", "firefox-source.tar.gz"))) {
+  const out = join(process.cwd(), ".warpfox", "firefox-source.tar.gz");
+  if (await exists(out)) {
     return;
   }
   logger.info(`Downloading Firefox v${version}`);
-  let bar = new ProgressBar("Downloading [:bar] :rate/bps (:percent)", {
-    total: 100 * 1000,
-    width: 40,
-  });
-  const downloader = new Downloader({
-    url,
-    directory: join(process.cwd(), ".warpfox"),
-    onProgress: function (percentage, chunk, remainingSize) {
-      bar.tick();
-    },
+  await downloadFileToLocation(url, out);
+}
+
+async function unpackFirefox(version: string) {
+  let tarExec = "tar";
+  const tmpDir = join(process.cwd(), ".warpfox");
+  const name = `firefox-source.tar.gz`;
+  const outDir = join(tmpDir, "engine");
+
+  // On MacOS, we need to use gnu tar, otherwise tar doesn't behave how we
+  // would expect it to behave, so this section is responsible for handling
+  // that
+  //
+  // If BSD tar adds --transform support in the future, we can use that
+  // instead
+  if (process.platform == "darwin") {
+    // GNU Tar doesn't come preinstalled on any MacOS machines, so we need to
+    // check for it and ask for the user to install it if necessary
+    if (!commandExistsSync("gtar")) {
+      throw new Error(
+        `GNU Tar is required to extract Firefox's source on MacOS. Please install it using the command |brew install gnu-tar| and try again`,
+      );
+    }
+
+    tarExec = "gtar";
+  }
+
+  if (await exists(resolve(tmpDir, "engine"))) {
+    return;
+  }
+
+  await mkdir(resolve(tmpDir, "engine"), { recursive: true });
+
+  logger.info("Unpacking firefox source");
+  if (process.platform === "win32") {
+    logger.info("Unpacking Firefox source on Windows (7z)");
+    await execa("7z", [
+      "x",
+      resolve(tmpDir, name),
+      "-o" + resolve(tmpDir, name.replace(".tar.xz", ".tar")),
+    ]);
+    logger.info("Unpacking Firefox source again without the .xz extension");
+    await execa("7z", [
+      "x",
+      resolve(tmpDir, name.replace(".tar.xz", ".tar")),
+      "-o" + tmpDir,
+    ]);
+    const archiveDir = resolve(tmpDir, "firefox-" + version);
+    logger.info("Moving Firefox source to engine directory");
+    // move the extracted files to the engine directory
+    await execa("mv", [archiveDir, outDir]);
+    return;
+  }
+
+  await execa(
+    tarExec,
+    ["--strip-components=1", "-xf", resolve(tmpDir, name), "-C", outDir].filter(
+      Boolean,
+    ) as string[],
+  );
+}
+
+async function initFirefoxRepo(ffVersion: string) {
+  logger.info("Initializing git, this may take some time");
+  const absoluteInitDirectory = join(process.cwd(), ".warpfox", "engine");
+
+  await execa("git", {
+    args: ["init"],
+    cwd: absoluteInitDirectory,
   });
 
-  try {
-    await downloader.download();
-  } catch (error) {
-    logger.error(`Failed to download Firefox v${version} (${error})`);
-    process.exit(1);
-  }
+  await execa("git", {
+    args: ["init"],
+    cwd: absoluteInitDirectory,
+  });
+
+  await execa("git", {
+    args: ["checkout", "--orphan", ffVersion],
+    cwd: absoluteInitDirectory,
+  });
+
+  await execa("git", {
+    args: ["add", "-f", "."],
+    cwd: absoluteInitDirectory,
+  });
+
+  logger.info("Committing...");
+
+  await execa("git", {
+    args: ["commit", "-aqm", `"Firefox ${ffVersion}"`],
+    cwd: absoluteInitDirectory,
+  });
+
+  await execa("git", {
+    args: ["checkout", "-b", "warpfox_" + ffVersion.replace(/\./g, "_")],
+    cwd: absoluteInitDirectory,
+  });
 }
 
 export default async function buildCommand({
@@ -39,8 +121,11 @@ export default async function buildCommand({
   manifest: Manifest;
   cache: Cache;
 }) {
-  await downloadFirefox(manifest.firefoxVersion);
+  const ffVersion = manifest.firefoxVersion;
+  await downloadFirefox(ffVersion);
+  await unpackFirefox(ffVersion);
   if (!cache.hasInitialised) {
-    //cache.hasInitialised = true;
+    await initFirefoxRepo(ffVersion);
   }
+  cache.hasInitialised = true;
 }
